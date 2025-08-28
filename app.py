d8b3e4f2de54c1e3d11209681e61bbaa8b2ca38a
stephantom <112913985+stephantom@users.noreply.github.com>
2025-08-28 12:51:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,5 @@ st.title(" Mi primer App!!")
 st.header ("En este espacio empiezo a desarrollar...")
 st.write("Facilmente puedo..")
 Image = Image.open("Fondo de Pantalla de Escritorio Motivational en Rojo Carbón estilo Antidiseño.png")
+
+st.image(image, caption= 'Fondo de Pantalla de Escritorio Motivational en Rojo Carbón estilo Antidiseño')
