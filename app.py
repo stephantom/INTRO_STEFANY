36eb05f2a85b87dc6896f295354c892603caff99
stephantom <112913985+stephantom@users.noreply.github.com>
2025-08-28 12:47:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
 import streamlit as st
+from PIL import Image
+
 st.title(" Mi primer App!!")
+st.header ("En este espacio empiezo a desarrollar...")
+st.write("Facilmente puedo..")
+Image = Image.open("Fondo de Pantalla de Escritorio Motivational en Rojo Carbón estilo Antidiseño")
