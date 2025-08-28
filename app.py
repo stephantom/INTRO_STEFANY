9eb03ca812b8e5ecd85ec1de3c727623d4c52dfd
stephantom <112913985+stephantom@users.noreply.github.com>
2025-08-28 12:56:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.header ("En este espacio empiezo a desarrollar...")
 st.write("Facilmente puedo..")
 Image = Image.open("Fondo de Pantalla de Escritorio Motivational en Rojo Carbón estilo Antidiseño.png")
 
-st.image(image, caption='Interfaces multimodales')
+st.Image(Image, caption='Interfaces multimodales')
 
 texto = st.text_input('Escribe algo','Este es mi texto')
 st.write('El texto escrito es', texto)
