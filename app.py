38a0c658fe3c7425664254ec01c185955df51d58
stephantom <112913985+stephantom@users.noreply.github.com>
2025-08-28 12:56:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,13 @@ import streamlit as st
 from PIL import Image
 
 st.title(" Mi primer App!!")
+
 st.header ("En este espacio empiezo a desarrollar...")
 st.write("Facilmente puedo..")
 Image = Image.open("Fondo de Pantalla de Escritorio Motivational en Rojo Carbón estilo Antidiseño.png")
 
-st.image(image, caption= 'Fondo de Pantalla de Escritorio Motivational en Rojo Carbón estilo Antidiseño.png')
+st.image(image, caption='Interfaces multimodales')
+
+texto = st.text_input('Escribe algo','Este es mi texto')
+st.write('El texto escrito es', texto)
+
